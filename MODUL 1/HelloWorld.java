1b1ff4491b42890a516d65eba1ae216e9b10d779
hadiansyach <hadiansyach@student.telkomuniversity.ac.id>
2021-09-27 12:10:41 +0700

    HelloWorld.java

---
+++ MODUL 1/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama	:Ihsan Hadiansyach");
+		System.out.println("NIM	: 6706213032") ;
+		System.out.println("Kelas	: D3RPLA-45-01") ;
+		
+	}
+}
